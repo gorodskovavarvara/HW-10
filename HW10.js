ea4596fe5ca10757e3019271b3d215dd20988fe6
gorodskovavarvara <gorodskova.varvara@gmail.com>
2020-02-11 10:57:47 +0200

    HomeWork

---
+++ HW10.js
@@ -0,0 +1,11 @@
+function OK(){
+    if(confirm("is all right?")) {
+        alert("Welcome!")
+    } 
+    else{
+        an.fio.value = "";
+        an.age.value = "";
+        an.tel.value = "";
+        an.mail.value = "";
+    }
+}
